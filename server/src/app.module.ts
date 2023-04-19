8b7d46349e836cab821171415c89a51bd7a01aed
tonny0831 <tonnyjansen0831@gmail.com>
2023-04-19 12:55:17 +0800

    chore(resource): initialize basic resources

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ActivityModule],
   controllers: [AppController],
   providers: [AppService],
 })
